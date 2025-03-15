d0f9df9688bcd23e42d295ccc341c6ba7bbf35e7
zainabhusain227 <41973371+zainabhusain227@users.noreply.github.com>
2025-03-15 14:32:34 -0400

    Assignment value to variable

--- Assets/Scenes/Tester.cs
+++ Assets/Scenes/Tester.cs
@@ -8,7 +8,7 @@ public class Tester : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        x = 3;
     }
 
     // Update is called once per frame
